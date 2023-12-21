5144dc886d771587713f3fe458d01f019fbe8348
Iagodino <iagovs@gmail.com>
2023-12-20 21:35:41 -0300

    descricao

--- HenryMod/Modules/StaticValues.cs
+++ HenryMod/Modules/StaticValues.cs
@@ -4,11 +4,12 @@ namespace HenryMod.Modules
 {
     internal static class StaticValues
     {
-        internal static string descriptionText = "Henry is a skilled fighter who makes use of a wide arsenal of weaponry to take down his foes.<color=#CCD3E0>" + Environment.NewLine + Environment.NewLine
-             + "< ! > Sword is a good all-rounder while Boxing Gloves are better for laying a beatdown on more powerful foes." + Environment.NewLine + Environment.NewLine
-             + "< ! > Pistol is a powerful anti air, with its low cooldown and high damage." + Environment.NewLine + Environment.NewLine
-             + "< ! > Roll has a lingering armor buff that helps to use it aggressively." + Environment.NewLine + Environment.NewLine
-             + "< ! > Bomb can be used to wipe crowds with ease." + Environment.NewLine + Environment.NewLine;
+        internal static string descriptionText = "Kratos, the Ghost of Sparta, is an unstoppable force that has emerged from the depths of Greek mythology. " +
+            "Driven by an unrelenting quest for redemption and vengeance, he carries with him the wrath of the gods and the determination of an indomitable warrior.<color=#CCD3E0>" + Environment.NewLine + Environment.NewLine
+             + "< ! > The Blades of Caos have the capability to blades to extends the reach of the attacks making a good close to mid range attack" + Environment.NewLine + Environment.NewLine
+             + "< ! > Leviathan is a powerful Axe given by Faye that helps you with anti-air attacks, freezes enemies, and deals a moderate damage." + Environment.NewLine + Environment.NewLine
+             + "< ! > These Wings of Icarus enable swift aerial dashes, enhancing Kratos' agility in both combat and locomotion." + Environment.NewLine + Environment.NewLine
+             + "< ! > With the Fury of the God of War you can gain buffs for your atributes and gain a temporary invincibilty." + Environment.NewLine + Environment.NewLine;
 
         internal const float swordDamageCoefficient = 2.8f;
 
